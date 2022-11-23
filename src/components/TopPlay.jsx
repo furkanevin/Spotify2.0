0f3d88f59fe3f0a0b76b7944f5628306007d1ac6
furkanevin <furkanevin00@gmail.com>
2022-11-23 19:00:12 +0300

    top artists update

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -20,7 +20,16 @@ const TopChartCard = ({ song, i }) => (
       />
       <div className="flex-1 flex flex-col justify-center mx-3 ">
         <Link to={`/songs/${song.key} `}>
-          <p className="text-xl font-bold text-white">{song?.title} </p>
+          <p className="text-xl font-bold text-white">
+            {song?.title.slice(0, 10)}
+          </p>
+        </Link>
+      </div>
+      <div className="flex-1 flex flex-col justify-center mx-3 ">
+        <Link to={`/artists/${song?.artists[0].adamid} `}>
+          <p className="text-base font-bold text-gray-300 mt-1">
+            {song?.subtitle}
+          </p>
         </Link>
       </div>
     </div>
