df7a268715c9feefd9e99b76f6336d082d46bcc1
furkanevin <furkanevin00@gmail.com>
2022-11-23 15:13:34 +0300

    top-play-update

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { Swiper, SwiperSlide, FreeMode } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
@@ -12,6 +13,11 @@ const TopPlay = () => {
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data } = useGetTopChartsQuery();
   const divRef = useRef(null);
+
+  useEffect(() => {
+    divRef.current.scrollIntoView();
+  });
+
   const topPlays = data?.slice(0, 5);
   const handlePlayClick = () => {
     dispatch(setActiveSong({ song, data, i }));
@@ -20,6 +26,7 @@ const TopPlay = () => {
   const handlePauseClick = () => {
     dispatch(playPause(false));
   };
+  return <div ref={divRef}></div>;
 };
 
 export default TopPlay;
