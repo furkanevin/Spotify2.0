d26acd1d083ab2fadab811119b8d82f144bb8b93
furkanevin <furkanevin00@gmail.com>
2022-11-23 15:19:56 +0300

    top-play-update

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -26,7 +26,21 @@ const TopPlay = () => {
   const handlePauseClick = () => {
     dispatch(playPause(false));
   };
-  return <div ref={divRef}></div>;
+  return (
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] mx-w-full flex flex-col"
+    >
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 cursor-pointer text-base">See more</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopPlay;
